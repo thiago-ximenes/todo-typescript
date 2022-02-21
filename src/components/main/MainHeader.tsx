d4eaad5de03bb5387adc3e5415aabd3eba3154e3
Thiago Ximenes <thiagoximeneslima@gmail.com>
2022-02-21 10:39:53 -0300

    install react-icons and style add task button

--- src/components/main/MainHeader.tsx
+++ src/components/main/MainHeader.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 
 function MainHeader() {
-
-  const date : Date = new Date()
-  const options : object  = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
-  const locale : string = date.toLocaleString('pt-BR', options)
+  const date = new Date()
+  enum Options { weekday = 'long', year = 'numeric', month = 'long', day = 'numeric' }
+  const locale = date.toLocaleString('pt-BR', Options)
 
   return (
     <header
--- src/components/main/MainInput.tsx
+++ src/components/main/MainInput.tsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { BsPlusCircleDotted } from 'react-icons/bs'
 
 function MainInput() {
+  const [inputTaskOn, setInputTaskOn] = useState<boolean>(false)
+
+  function handleAddTaskClick(e : React.MouseEvent<HTMLButtonElement>) {
+    e.preventDefault()
+    setInputTaskOn(!inputTaskOn)
+  }
+
   return (
-    <div>MainInput</div>
+    !inputTaskOn ?
+    <button
+      className="btn btn-outline-secondary d-flex mx-auto
+      justify-content-center align-items-center fw-bold fs-4"
+      type="button"
+      onClick={handleAddTaskClick}
+    >
+      Adicionar tarefa
+      <div
+        className="ms-2 d-flex align-items-center text-danger"
+      >
+        <BsPlusCircleDotted />
+      </div>
+    </button>
+    :
+    (
+      <div>Funfou</div>
+    )
   )
 }
 
